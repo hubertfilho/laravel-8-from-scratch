085a53cc829babd8b3dc651d1a48a2e2bb359af0
Hubert Streich Filho <hubert.stf@gmail.com>
2024-03-20 12:00:26 -0300

    Some improvements in Blade template and adding the findOrFail method for finding a post.

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,24 +1,14 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="/app.css">
-        <title>Post</title>
-    </head>
-    <body>
-        <main>
-            <article>
-                <h1>
-                    <?= $post->title; ?>
-                </h1>
-                <h2><?= $post->excerpt; ?></h2>
-                <div>
-                    <?= $post->body; ?>
-                </div>
-            </article>
-
-            <a href="/">Go back</a>
-        </main>
-    </body>
-</html>
+<x-layout>
+    <x-slot name="content">
+        <article>
+            <h1>
+                {{ $post->title }}
+            </h1>
+            <h2>{{ $post->excerpt }}</h2>
+            <div>
+                {!! $post->body !!}
+            </div>
+        </article>
+        <a href="/">Go back</a>
+    </x-slot>
+</x-layout>
