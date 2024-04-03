9d7d17e7646e637f6c226a3b61bfb47feb2489fc
Hubert Streich Filho <hubert.stf@gmail.com>
2024-04-03 17:36:15 -0300

    Wrote Category and Post factories; Changed the DatabaseSeeder to use the factories.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,48 +15,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'title' => 'Personal',
-            'slug' => 'personal',
-        ]);
-        $work = Category::create([
-            'title' => 'Work',
-            'slug' => 'work',
-        ]);
-        $hobbies = Category::create([
-            'title' => 'Hobbies',
-            'slug' => 'hobbies',
+        $user = User::factory()->create([
+            'name' => 'Hubert Filho'
         ]);
 
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My personal post',
-            'slug' => 'my-personal-post',
-            'excerpt' => 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis nibh sed tellus rhoncus feugiat ut eget nunc. Praesent vestibulum efficitur orci, sit amet auctor risus malesuada non. Donec accumsan congue nunc, at placerat enim volutpat id. Donec a pulvinar quam. Praesent purus nunc, tempus ut massa tincidunt, sollicitudin condimentum odio. Mauris euismod vulputate mi quis tincidunt. Maecenas quis porta felis. Nam sed orci et quam rhoncus imperdiet facilisis nec nisl. Integer non lectus nulla. Sed bibendum lacus nec sapien porttitor cursus. Duis id volutpat leo, vitae eleifend ex. Aliquam nec imperdiet lectus. Etiam augue ligula, rhoncus sit amet lectus eu, gravida pulvinar ligula. Phasellus nec velit at risus ultricies vehicula vitae nec ipsum. Duis aliquam varius mauris quis scelerisque.</p>',
+        $category = Category::factory()->create();
+
+        Post::factory(3)->create([
+            'user_id' => $user,
+            'category_id' => $category->id,
         ]);
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My work post',
-            'slug' => 'my-work-post',
-            'excerpt' => 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis nibh sed tellus rhoncus feugiat ut eget nunc. Praesent vestibulum efficitur orci, sit amet auctor risus malesuada non. Donec accumsan congue nunc, at placerat enim volutpat id. Donec a pulvinar quam. Praesent purus nunc, tempus ut massa tincidunt, sollicitudin condimentum odio. Mauris euismod vulputate mi quis tincidunt. Maecenas quis porta felis. Nam sed orci et quam rhoncus imperdiet facilisis nec nisl. Integer non lectus nulla. Sed bibendum lacus nec sapien porttitor cursus. Duis id volutpat leo, vitae eleifend ex. Aliquam nec imperdiet lectus. Etiam augue ligula, rhoncus sit amet lectus eu, gravida pulvinar ligula. Phasellus nec velit at risus ultricies vehicula vitae nec ipsum. Duis aliquam varius mauris quis scelerisque.</p>',
-        ]);
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $hobbies->id,
-            'title' => 'My hobbies post',
-            'slug' => 'my-hobbies-post',
-            'excerpt' => 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis nibh sed tellus rhoncus feugiat ut eget nunc. Praesent vestibulum efficitur orci, sit amet auctor risus malesuada non. Donec accumsan congue nunc, at placerat enim volutpat id. Donec a pulvinar quam. Praesent purus nunc, tempus ut massa tincidunt, sollicitudin condimentum odio. Mauris euismod vulputate mi quis tincidunt. Maecenas quis porta felis. Nam sed orci et quam rhoncus imperdiet facilisis nec nisl. Integer non lectus nulla. Sed bibendum lacus nec sapien porttitor cursus. Duis id volutpat leo, vitae eleifend ex. Aliquam nec imperdiet lectus. Etiam augue ligula, rhoncus sit amet lectus eu, gravida pulvinar ligula. Phasellus nec velit at risus ultricies vehicula vitae nec ipsum. Duis aliquam varius mauris quis scelerisque.</p>',
-        ]);
+
+        Post::factory(2)->create();
     }
 }
