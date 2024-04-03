dacd2e177a1b3ed26b5d8dec7c953da98350fc59
Hubert Streich Filho <hubert.stf@gmail.com>
2024-04-03 18:25:04 -0300

    Eager loading categories and authors in posts.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,6 +16,8 @@ class Post extends Model
         'body',
     ];
 
+    protected $with = ['category', 'author'];
+
     public function category()
     {
         return $this->belongsTo(Category::class);
